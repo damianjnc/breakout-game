db440014b0377f6e74e23d64222dce2a1ddef267
damianjnc <damianjnc@gmail.com>
2019-07-29 20:05:11 +0100

    first commit

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const getRange = length => [...Array(length).keys()];
+export const getRandomFrom = (...args) =>
+  args[Math.floor(Math.random() * args.length)];
+export const flatten = arrays =>
+  arrays.reduce((acc, row) => [...acc, ...row], []);
